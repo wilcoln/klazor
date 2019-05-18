8ef8b81f4672e387ebe362346a9dd94fbcd5de92
Wilfried Bounsi <wilcoln99@gmail.com>
2019-05-18 04:27:31 +0200

    Enables frontend image content insertion, update and delete

--- static/js/contents.js
+++ static/js/contents.js
@@ -1,33 +1,37 @@
-    class Content {
-        constructor(id){
-            this.id = id
-        }
+class Content {
+    constructor(id) {
+        this.id = id
     }
-    class VideoContent extends  Content{
-        constructor(id, title, video){
-            super(id)
-            this.title = title
-            this.video = video
-        }
-    }
-    class AudioContent extends  Content{
-        constructor(id, title, audio){
-            super(id)
-            this.title = title
-            this.audio = audio
-        }
-    }
-    class ImageContent extends Content{
-            constructor(id, title, image){
-                super(id)
-                this.title = title
-                this.image = image
-            }
+}
 
+class VideoContent extends Content {
+    constructor(id, title, video) {
+        super(id)
+        this.title = title
+        this.video = video
     }
-    class MarkdownContent extends Content{
-            constructor(id, text){
-                super(id)
-                this.text = text
-            }
+}
+
+class AudioContent extends Content {
+    constructor(id, title, audio) {
+        super(id)
+        this.title = title
+        this.audio = audio
     }
+}
+
+class ImageContent extends Content {
+    constructor(id, title, image) {
+        super(id)
+        this.title = title
+        this.image = image
+    }
+
+}
+
+class MarkdownContent extends Content {
+    constructor(id, text) {
+        super(id)
+        this.text = text
+    }
+}
