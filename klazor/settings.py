f92fbba44bcc6acb06e1749762c7878f3e0b7d36
Wilfried Bounsi <wilcoln99@gmail.com>
2019-05-26 10:05:11 +0200

    Add Resources and Instructors list and the bottom of course items

--- klazor/settings.py
+++ klazor/settings.py
@@ -121,13 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'node_modules')
 )
 
+STATIC_ROOT = BASE_DIR
 MEDIA_ROOT = BASE_DIR + '/media/'
-
-MEDIA_URL = '/media/'
 
 DATA_UPLOAD_MAX_MEMORY_SIZE = 500242880
