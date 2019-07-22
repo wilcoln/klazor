50c3231d3c4c683e58b5d0e8f163db0f3b56aeb9
Wilfried Bounsi <wilcoln99@gmail.com>
2019-07-22 16:24:27 +0200

    add last modified mention to sheet component

--- static/js/sheet.js
+++ static/js/sheet.js
@@ -1,6 +1,7 @@
 // Navbr and toolbar animation
 onscroll = function () {
     let sheetToolbar = $(".sheet-toolbar");
+    let modificationInfo = $("#info-modification");
     if (scrollY > 40) {
         sheetToolbar.css('background', 'white')
     } else {
